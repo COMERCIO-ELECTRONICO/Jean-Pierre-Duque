fbe644d19201b586f6c7220aecdca158a42cc05f
Jean Pierre Duque <jpduquedoe@gmail.com>
2020-05-22 13:43:45 -0500

    repaso 2 rutas, botones y estilos

--- ejercicio/src/app/app.module.ts
+++ ejercicio/src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { AppRoutes } from './app.routes';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NoEncontradoComponent } from './no-encontrado/no-encontrado.component';
 import { IniciarSesionComponent } from './iniciar-sesion/iniciar-sesion.component';
 import { LoginComponent } from './login/login.component';
+import { AppRoutes } from './app.routes';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
@@ -15,13 +16,10 @@ import { LoginComponent } from './login/login.component';
     HomeComponent,
     NoEncontradoComponent,
     IniciarSesionComponent,
-    LoginComponent
+    LoginComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutes
-  ],
+  imports: [BrowserModule, AppRoutes, BrowserAnimationsModule, ButtonModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
